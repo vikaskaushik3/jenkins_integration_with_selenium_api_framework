31ba6c27d355c4d6b39f105510d6391ac00d8751
Vikaas <vikaskaushik166@gmail.com>
2025-03-26 13:37:11 +0530

    updated conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
-
+# fixture for browser driver
 @pytest.fixture(scope="class")
 def browser(request):
     options = webdriver.ChromeOptions()
